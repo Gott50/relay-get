474bc5cefce9fca69cd355d34f9d12f7ad7c8abe
Gott50 <xxGott50xx@gmail.com>
2018-07-22 09:49:07 +0200

    relay GET Requests from given Query

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var index = req.originalUrl.indexOf("=");
+    var query = req.originalUrl.substring(index + 1);
+
+    request(query).then(s => res.send(s))
+        .catch(err => next(err));
 });
+
+async function request(query) {
+    let response = await fetch(query);
+    let text = await parsResponse(response);
+    return JSON.parse(text);
+}
+
+async function parsResponse(response) {
+    let text = await response.text();
+    if (response.status === 200) return text;
+    else return new Error(text);
+}
 
 module.exports = router;

9fb679fd3351e7068ec475acfa83e5453f592863
Gott50 <xxGott50xx@gmail.com>
2018-07-22 11:08:29 +0200

    extract relayRequest

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-async function getSuggestion(query) {
-    let url =
-        'https://www.instagram.com/web/search/topsearch/?context=blended&' +
-        'query=' + query;
-    let response = await fetch(url);
-    let text = await parsResponse(response);
-    let json = JSON.parse(text);
-    return json;
-}
-
-async function parsResponse(response) {
-    let text = await response.text();
-    if (response.status === 200) return text;
-    else return new Error(text);
-}
-
-getSuggestion("cat").then(s => {
+relayRequest(location.search).then(s => {
     console.log(s);
     document.body.innerHTML = JSON.stringify(s);
 });
